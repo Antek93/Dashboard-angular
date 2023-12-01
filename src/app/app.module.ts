b136262e1c875425c01e7eb45a97b2327cb23f5e
Antek93 <antonio.savino1993@gmail.com>
2023-12-01 16:01:07 +0100

    Fixed Routing and components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './componenti/login/login.component';
+import { RegisterComponent } from './componenti/register/register.component';
+import { DashboardComponent } from './componenti/dashboard/dashboard.component';
+import { Pagina1Component } from './componenti/pagina1/pagina1.component';
+import { Pagina2Component } from './componenti/pagina2/pagina2.component';
+import { Pagina3Component } from './componenti/pagina3/pagina3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    Pagina1Component,
+    Pagina2Component,
+    Pagina3Component
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
